4e576c5a1effbff9c1c585c2bf3581fb6bc713d7
nazdum <64675616+nazdum@users.noreply.github.com>
2025-04-22 19:54:16 -0500

    updated front-end to show response status code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,28 +3,40 @@ import './App.css';
 import {Request} from "../wailsjs/go/main/App";
 
 function App() {
-    const [resultText, setResultText] = useState("");
-    const [name, setName] = useState('');
-    const updateName = (e) => setName(e.target.value);
-    const updateResultText = (result) => setResultText(result);
 
- 
-    const request = ()=>{
-        Request(name).then(updateResultText)
+    const [resultText, setResultText] = useState('');
+    const [statusCode, setStatusCode] = useState('Status');
+    const [url, setUrl] = useState('');
+
+    const updateUrl = (e) => setUrl(e.target.value);
+
+    const request = async () =>{
+        try{
+            const response = await Request(url);
+            setResultText(response.Body);
+            setStatusCode(response.StatusCode);
+        }catch(error){
+            console.log(error);
+        }
     }
+
+    const statusCodeStyle = {
+        color: statusCode === 200 ? 'green' : (statusCode === 404 ? 'red' : 'black')
+    };
 
     return (
         <div className="app">
 
             <div className="inputs">
-            <input placeholder="https://www.test.com/api/" id="url" onChange={updateName} type="text"/>
+            <input placeholder="https://www.test.com/api/" id="url" onChange={updateUrl} type="text"/>
             <button id="request-button" onClick={request}>Request</button>
             </div>
 
             <div className="outputs">
+            <h3 style={statusCodeStyle}>{statusCode}</h3>
             <textarea readOnly id="response-textarea" value={resultText} />
             </div>
-            
+
         </div>
     )
 }
